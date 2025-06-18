f591fa6539c289decf08fb476283ec71ef095897
harkav <harald_kavli@hotmail.com>
2025-06-18 16:00:16 +0200

    add compound method

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from numbers import Number 
+
 def main():
     print("Hello from compound-streamlit!")
 
 
 if __name__ == "__main__":
     main()
+
+
+def calculate_compound(principal : Number, assumed_yearly_profit : Number, num_years : Number ) -> float: 
+    """
+    Returns the assumed profit for an investment based on assumed yearly profit and number of years.
+    
+    Args: 
+        principal (Number): The investment as a number.
+        assumed_yearly_profit(Number): the assumed growth rate (if you care about inflation, just deduct that)
+        num_years (Number): years of investment.
+        
+    Returns: 
+        The sum (float).
+    
+    """
+    
+    return principal * (1 + (assumed_yearly_profit / 100) ** num_years)
+    
